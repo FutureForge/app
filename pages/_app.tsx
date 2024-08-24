ac2d92e934803f2f40e9e0df5c42f88e712e84da
Joseph <oojoseph67@gmail.com>
2024-08-24 14:14:14 +0100

    thirdweb and react-query setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,16 @@
 import "@/styles/globals.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import type { AppProps } from "next/app";
+import { ThirdwebProvider } from "thirdweb/react";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const queryClient = new QueryClient();
+
+  return (
+    <ThirdwebProvider>
+      <QueryClientProvider client={queryClient}>
+        <Component {...pageProps} />
+      </QueryClientProvider>
+    </ThirdwebProvider>
+  );
 }
---
+++ utils/index.ts
@@ -0,0 +1,7 @@
+import { createThirdwebClient } from "thirdweb";
+
+const clientId = "21c145464870191d752d334f06abcb73";
+
+export const client = createThirdwebClient({
+  clientId,
+});
